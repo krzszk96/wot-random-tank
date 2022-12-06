8c93de57890042cba46feb5ae2c95a75e4226b54
krzysztof <krzszk96@gmail.com>
2022-12-06 02:04:07 +0100

    working rolling and reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,7 @@ import { DataService } from './services/data.service';
 export class AppComponent implements OnInit {
   availableTanks!: Array<Tank>;
   scrollTanks!: Array<ElementRef>;
+  hideview: boolean = true;
   @ViewChildren('tank') tanks!: QueryList<ElementRef>;
 
   constructor(private dataService: DataService) {}
@@ -30,21 +31,63 @@ export class AppComponent implements OnInit {
           name: data[tank].name,
           imgUrl: data[tank].images.big_icon,
         }));
-        console.log(this.availableTanks);
+        this.shuffle(this.availableTanks);
         this.createTanksRef();
       });
+  }
+
+  hideView(): void {
+    this.hideview = !this.hideview;
   }
 
   createTanksRef(): void {
     this.tanks.changes.subscribe((tank) => {
       this.scrollTanks = tank.toArray();
+      console.log(this.scrollTanks);
     });
   }
 
-  roll(target: number): void {
+  clearOpacityStyles(): void {
+    this.scrollTanks.forEach((tank) => {
+      tank.nativeElement.setAttribute('style', 'opacity:1');
+    });
+  }
+
+  roll(): void {
+    let target = this.availableTanks!.length - 5;
     this.scrollTanks[target].nativeElement.scrollIntoView({
       behavior: 'smooth',
     });
+    this.changeStyleSurroundingofTanks(target);
+    console.log(this.availableTanks![target + 2].name);
+  }
+
+  reset(): void {
+    this.shuffle(this.availableTanks);
+    this.scrollTanks[0].nativeElement.scrollIntoView({
+      behavior: 'smooth',
+    });
+    this.clearOpacityStyles();
+  }
+
+  shuffle(array: Array<Tank>): Array<Tank> {
+    let currentIndex = array.length,
+      randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  changeStyleSurroundingofTanks(target: number): void {
     this.scrollTanks[target].nativeElement.setAttribute('style', 'opacity:0.2');
     this.scrollTanks[target + 1].nativeElement.setAttribute(
       'style',
